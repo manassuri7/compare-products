248f5a0a17b75bff5b951454f9c7320d25fe0728
manassuri7 <manassuri7@outlook.com>
2020-01-16 00:12:58 +0530

    Updates

---
+++ precache-manifest.62afefc46f1a0abff5c0d3a1838bb80c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7598d3aa6a37c514250902792fc3bb32",
+    "url": "/compare-products/index.html"
+  },
+  {
+    "revision": "00055ee8a28afc00a2d0",
+    "url": "/compare-products/static/css/2.2ec3db0d.chunk.css"
+  },
+  {
+    "revision": "33f6c8ac7fff5440bd38",
+    "url": "/compare-products/static/css/main.40b6af21.chunk.css"
+  },
+  {
+    "revision": "00055ee8a28afc00a2d0",
+    "url": "/compare-products/static/js/2.b244b1fe.chunk.js"
+  },
+  {
+    "revision": "070a35f007a0d6bcc38c5a4f0360abe1",
+    "url": "/compare-products/static/js/2.b244b1fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "33f6c8ac7fff5440bd38",
+    "url": "/compare-products/static/js/main.2c517c2b.chunk.js"
+  },
+  {
+    "revision": "8aeaa03d8512bf1f5278",
+    "url": "/compare-products/static/js/runtime-main.4d31902a.js"
+  }
+]);
